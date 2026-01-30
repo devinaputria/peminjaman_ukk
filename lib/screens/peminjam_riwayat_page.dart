7b3a911f595b48bf8128d9c711db089765ea4313
devinaputria <devina@example.com>
2026-01-30 08:36:42 +0700

    Update RiwayatPage UI: AppBar + Card modern

--- lib/screens/peminjam_riwayat_page.dart
+++ lib/screens/peminjam_riwayat_page.dart
@@ -1,37 +1,21 @@
 import 'package:flutter/material.dart';
 
-class RiwayatPage extends StatelessWidget {
-  const RiwayatPage({super.key});
+class PeminjamRiwayatPage extends StatelessWidget {
+  const PeminjamRiwayatPage({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Riwayat Peminjaman')),
-      body: ListView(
-        padding: const EdgeInsets.all(16),
-        children: [
-          const Text('Dipinjam', style: TextStyle(fontWeight: FontWeight.bold)),
-          Card(
-            child: ListTile(
-              title: const Text('Mesin Gerinda'),
-              subtitle: const Text('Status: Dipinjam'),
-            ),
-          ),
-
-          const SizedBox(height: 16),
-
-          const Text('Dikembalikan', style: TextStyle(fontWeight: FontWeight.bold)),
-          Card(
-            child: ListTile(
-              title: const Text('Mesin Bor'),
-              subtitle: const Text('Status: Dikembalikan'),
-            ),
-          ),
-        ],
+      appBar: AppBar(
+        title: const Text('Riwayat Peminjaman'),
+        centerTitle: true,
+      ),
+      body: const Center(
+        child: Text(
+          'Riwayat masih kosong',
+          style: TextStyle(fontSize: 18, color: Colors.grey),
+        ),
       ),
     );
   }
 }
-
-
- 
